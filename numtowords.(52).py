08eeed0be8764a53b91929d67dbe02c9a4761d5e
Akkash S <51949108+Akkash02@users.noreply.github.com>
2019-07-07 17:31:24 +0530

    Update numtowords.(52).py

--- numtowords.(52).py
+++ numtowords.(52).py
@@ -1,3 +1,3 @@
-a=int(input())
-b=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
-print(b[a])
+b=int(input())
+a=['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten']
+print(a[b])
